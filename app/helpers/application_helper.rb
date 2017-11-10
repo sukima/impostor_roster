71e336b257802517bcd7ea53eb80ccb2b085b862
Devin Weaver <suki@tritarget.org>
2017-11-10 00:24:21 -0500

    Display times with humanize_months helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  def humanize_months(months)
+    return "less then a month" if months == 0
+    years = (months / 12).floor
+    remainder = months % 12
+    result = []
+    if years > 1
+      result << "#{years} years"
+    elsif years > 0
+      result << "1 year";
+    end
+    if remainder > 1
+      result << "#{remainder} months"
+    elsif remainder > 0
+      result << "1 month"
+    end
+    result.join(" ")
+  end
 end
